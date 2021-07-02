ae4f0454436f94c74df9f445476b12a0733cf5b9
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-07-02 23:14:12 +1200

    Initial Commit

---
+++ TestingGrounds/TestingGroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingGroundsApp.swift
+//  TestingGrounds
+//
+//  Created by Lorenzo Zemp on 2/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TestingGroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
