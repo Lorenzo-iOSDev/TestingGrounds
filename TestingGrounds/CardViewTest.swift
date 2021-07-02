3c44541854ba52b6a77f125b6fac37ea71b97414
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2021-07-03 00:14:53 +1200

    CardView test, testing out card views that has drop shadows. Might get to use in next app.

---
+++ TestingGrounds/CardViewTest.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  TestingGrounds
+//
+//  Created by Lorenzo Zemp on 2/07/21.
+//
+
+import SwiftUI
+
+struct CardViewTest: View {
+    var body: some View {
+        ZStack {
+            Color(.lightGray)
+                .ignoresSafeArea()
+            
+//            ScrollView{
+//                ForEach(1..<20) { _ in
+//                    CardView()
+//                }
+//            }
+            CardView()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardViewTest()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "tv")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 60, height: 60)
+            
+            VStack(alignment: .leading) {
+                Text("Apple TV+")
+                    .font(.title2)
+                    .fontWeight(.medium)
+                
+                Text("Monthly")
+                    .font(.body)
+                    .italic()
+            }.padding(.leading)
+            Spacer()
+            
+            
+            Text("$69.99")
+                .font(.title3)
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 10.0)
+                .foregroundColor(Color(.systemBackground))
+                .shadow(radius: 10))
+        .padding()
+    }
+}
